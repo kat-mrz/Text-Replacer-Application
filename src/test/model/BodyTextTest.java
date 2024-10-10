81485cdd6593d6d92229605696cebd1127f5caa0
Kat <kmrzljak@student.ubc.ca>
2024-10-10 02:27:30 -0700

    Added BodyTextTest and ReplacePairTest classes.

---
+++ src/test/model/BodyTextTest.java
@@ -0,0 +1,31 @@
+package model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BodyTextTest {
+    private BodyText b;
+
+    @BeforeEach
+    void runBefore() {
+        //stub
+    }
+
+    @Test
+    void testConstructor() {
+        //stub
+    }
+
+    @Test
+    void testReplace() {
+        //stub
+    }
+
+    @Test
+    void testGetText() {
+        //stub
+    }
+
+}
---
+++ src/test/model/ReplacePairTest.java
@@ -0,0 +1,40 @@
+package model;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ReplacePairTest {
+    private ReplacePair rp; 
+
+    @BeforeEach
+    void runBefore() {
+        //stub
+    }
+
+    @Test
+    void testConstructor() {
+        //stub
+    }
+
+    @Test
+    void testGetReplacer() {
+        //stub
+    }
+
+    @Test
+    void testGetReplacee() {
+        //stub
+    }
+
+    @Test
+    void testSetReplacer() {
+        //stub
+    }
+
+    @Test
+    void testSetReplacee() {
+        //stub
+    }
+    
+}
