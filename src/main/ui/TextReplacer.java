71cbb13e64647e197ea818d4fdf00ec78937a61d
Kat <kmrzljak@student.ubc.ca>
2024-10-10 14:04:27 -0700

    Implemented newBodyText and newReplacePair

--- src/main/ui/TextReplacer.java
+++ src/main/ui/TextReplacer.java
@@ -9,14 +9,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TextReplacer {
-    private List<ReplacePair> rp;
+    private List<ReplacePair> repPairs;
     private Scanner input;
+    private BodyText bt;
 
     //EFFECTS: instantiates the TextReplacer application
     public TextReplacer() {
         init();
-
-        newBodyText();
+        
+        newBodyText(bt);
         newReplacePair();
         
     }
@@ -25,15 +26,23 @@ public class TextReplacer {
     //EFFECTS: initializes scanner and list of replace pairs.
     public void init() {
         this.input = new Scanner(System.in);
-        this.rp = new ArrayList<>();
+        this.repPairs = new ArrayList<>();
     }
 
-    public void newBodyText() {
-        //stub
+    public void newBodyText(BodyText bt) {
+        System.out.println("Enter your body text below this line.");
+        String text = this.input.nextLine();
+        this.bt = new BodyText(text);
     }
 
     public void newReplacePair() {
-        //stub
+        System.out.println("Enter the word you would like to replace below this line.");
+        String replacee = this.input.nextLine();
+        System.out.println("Enter the word you would like to replace it with below this line.");
+        String replacer = this.input.nextLine();
+
+        ReplacePair rp = new ReplacePair(replacee, replacer);
+        this.repPairs.add(rp);
     }
 
 
