2d47cab75e02eb1ce33e589c79b0356ad00bf6b2
Kat <kmrzljak@student.ubc.ca>
2024-10-10 10:56:51 -0700

    Added and implemented getContainsReplacee method and implemented replace method.

--- src/main/model/BodyText.java
+++ src/main/model/BodyText.java
@@ -1,23 +1,34 @@
 package model;
 
+
 // Represents a body of text that has text.
 public class BodyText {
     private String text;
+    private ReplacePair r;
 
     //EFFECTS: constructs a body of text containing text
     public BodyText(String text) {
         this.text = text;
     }
 
-    //REQUIRES: text.contains(Replacer.replacee)
+    //REQUIRES: getContainsReplacee() == true
     //MODIFIES: this
     //EFFECTS: replaces every occurence of replacee in text with replacer.
-    public void replace() {
-        //stub
+    public void replace(ReplacePair r) {
+        text = text.replace(r.getReplacee(), r.getReplacer());
     }
 
     public String getText() {
         return this.text;
+    }
+
+    public boolean getContainsReplacee(ReplacePair r) {
+        if (text.contains(r.getReplacee())) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
     
 }
