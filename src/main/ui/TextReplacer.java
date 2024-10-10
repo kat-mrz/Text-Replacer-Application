e4e21e8b31081322c4b22a47c2633708b0118577
Kat <kmrzljak@student.ubc.ca>
2024-10-10 13:16:43 -0700

    Imported Scanner, Array List, and List to TextReplacer class. Documented and added TextReplacer and init classes. Added newBodyText and newReplacePair classes.

--- src/main/ui/TextReplacer.java
+++ src/main/ui/TextReplacer.java
@@ -3,6 +3,40 @@ package ui;
 import model.ReplacePair;
 import model.BodyText;
 
+
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
 public class TextReplacer {
-    
+    private List<ReplacePair> rp;
+    private Scanner input;
+
+    //EFFECTS: instantiates the TextReplacer application
+    public TextReplacer() {
+        init();
+
+        newBodyText();
+        newReplacePair();
+        
+    }
+
+    //MODIFIES: this
+    //EFFECTS: initializes scanner and list of replace pairs.
+    public void init() {
+        this.input = new Scanner(System.in);
+        this.rp = new ArrayList<>();
+    }
+
+    public void newBodyText() {
+        //stub
+    }
+
+    public void newReplacePair() {
+        //stub
+    }
+
+
+
+
 }
