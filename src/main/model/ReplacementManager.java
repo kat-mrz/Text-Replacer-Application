1a4f186944aa98cd9e7733cd4ef1b45ca7b47717
Kat <kmrzljak@student.ubc.ca>
2024-10-27 17:52:11 -0700

    Added ReplacementManager class and implemented methods.

---
+++ src/main/model/ReplacementManager.java
@@ -0,0 +1,42 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReplacementManager {
+    private List<ReplacePair> repPairs;
+    private ReplacePair possibleRepPair;
+
+    // EFFECTS: constructs a replacement manager with a list of replace pairs and a possible replace pair.
+    public ReplacementManager() {
+        this.repPairs = new ArrayList<>();
+        this.possibleRepPair = null;
+    }
+
+    // EFFECTS: adds a replace pair to the list of replace pairs.
+    // MODIFIES: this
+    public void addRepPair(ReplacePair rp) {
+        repPairs.add(rp);
+    }
+
+    public void setPossibleRepPair(ReplacePair rp) {
+        possibleRepPair = rp;
+    }
+
+    public ReplacePair getPossibleRepPair() {
+        return possibleRepPair;
+    }
+
+    public List<ReplacePair> getRepPairs() {
+        return repPairs;
+    }
+
+    public ReplacePair getLastRep() {
+        if (repPairs.size() > 0) {
+            return repPairs.getLast();
+        } else {
+            return null;
+        }
+        
+    }    
+}
